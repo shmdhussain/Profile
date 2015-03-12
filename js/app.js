563a8d0d166f481f5699a590c83dc06bf8260c06
Mohamed Hussain S H <shmdhussain12@gmail.com>
2015-03-12 09:47:40 +0530

    added css changes

---
+++ js/app.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+	$('.showprof button').click(function(){
+		if($(this).find('span').hasClass('glyphicon-plus')){
+			$(this).html('<span class="glyphicon glyphicon-minus" aria-hidden="true"></span> Hide Profile Photo');
+		}
+		else{
+			$(this).html('<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Show Profile Photo');
+		}
+	});
+
+});
